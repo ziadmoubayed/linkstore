93a5f1b64d927f50d4d534b34f7c482f98277475
Ziad <ziadmoubayed@gmail.com>
2019-12-20 12:08:56 -0500

    added headers test case for csv formatter

---
+++ src/test/java/TestCSVFormatter.java
@@ -0,0 +1,19 @@
+import com.github.newswhip.linkstore.common.CommonUtils;
+import com.github.newswhip.linkstore.common.Constants;
+import com.github.newswhip.linkstore.service.LinkScoreService;
+import com.github.newswhip.linkstore.service.impl.CSVReportFormatter;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestCSVFormatter {
+
+    @Test
+    public void resultShouldContainHeaders() {
+        var report = LinkScoreService.INSTANCE.getDomainStats();
+        var formattedLines = new CSVReportFormatter().formatReport(report);
+        assertTrue(formattedLines.size() > 0);
+        assertEquals(formattedLines.get(0), Constants.CSV_HEADERS);
+    }
+}
